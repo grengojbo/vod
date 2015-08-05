809182769405301719e5910d53fb04d5cb1e132d
Oleg Dolya <oleg.dolya@gmail.com>
2015-08-05 15:58:03 +0300

    изменено:      README.md 	изменено:      server.go

--- README.md
+++ README.md
@@ -2,3 +2,33 @@ vod
 ===
 
 Video on Demand 
+
+кодируем
+High profile H264 — 3 качества (X1.mp4 X2.mp4 X3.mp4)
+Baseline H.264 — 2 качества (X4.mp4 X5.mp4)
+
+под flash — отдаем OpenHttpStreamer'ом X1.mp4 X2.mp4 X3.mp4
+под iphone — ErlyVideo X4.mp4 X5.mp4
+под iPad — ErlyVideo X1.mp4 X2.mp4 X3.mp4 (там мощный чип декодирования и экран большой)
+под android phone — http progressive download c ручным переключением X4.mp4 X5.mp4
+под android pad — http progressive download c ручным переключением X1.mp4 X2.mp4 X3.mp4
+
+MPEG-DASH GStreamer
+gst-launch-1.0 -v -m v4l2src ! video/x-raw,width=640,height=480 \
+        ! x264enc tune=zerolatency ! "video/x-h264,profile=baseline" ! h264parse \
+        ! flvmux streamable=true name=mux \
+        alsasrc ! queue ! audioconvert ! voaacenc ! aacparse ! mux. \
+        mux. ! rtmpsink location=rtmp://localhost/myapp/mystream
+
+И еще трансляцию разрезаю по битрейтам — вот пример
+ffmpeg -i "rtsp://10.1.2.71/play1.sdp" -threads 2 -vcodec libx264 -preset UltraFast -rtbufsize 10000k -analyzeduration 0 -tune zerolatency -s 640x480 -acodec libmp3lame -ab 24k -ar 44100 -f flv "rtmp://127.0.0.1/live/it"
+
+вещания с локальной веб-камеры
+ffmpeg -f video4linux2 -i /dev/video0 -c:v libx264 -an -f flv rtmp://localhost/myapp/mystream
+
+вещаем с веб-камеры с низкой задержкой без звука, рисуем в верхней части картинки текущее время
+ffmpeg -r 25 -rtbufsize 1000000k -analyzeduration 0 -s vga -copyts -f dshow -i video="Webcam C170" -vf "drawtext=fontfile=verdana.ttf:fontcolor=yellow@0.8:fontsize=48:box=1:boxcolor=blue@0.8:text=%{localtime}" -s 320x240 -c:v libx264 -g 10 -keyint_min 1 -preset UltraFast -tune zerolatency -crf 25 -an -r 3 -f flv "rtmp://1.2.3.4:1935/live/b.flv live=1"
+
+Wirecast
+
+
--- server.go
+++ server.go
@@ -1,9 +1,30 @@
 package vod
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/astaxie/beego/httplib"
+)
+
 type Channels struct {
-	Name      string `json:"name"`
-	DirConfig string `json:"conf"`
-	DirTs     string `json:"ts"`
+	Id          int       `json:"id"`
+	Name        string    `json:"name"`
+	DirConfig   string    `json:"conf"`
+	DirTs       string    `json:"ts"`
+	DirSrc      string    `json:"src"`
+	DirVideoVod string    `json:"vod"`
+	Files       []string  `json:"files"`
+	Transcode   []string  `json:"transcode"`
+	Count       int       `json:"count"`
+	CountTrans  int       `json:"count_trans"`
+	Errors      ErrorMess `json:"errors"`
+	ApiUrl      string    `json:"apiUrl"`
+	Format      string    `json:"format"`
+	Size        string    `json:"size"`
 }
 
 type VodServer struct {
@@ -14,9 +35,129 @@ type VodServer struct {
 	channels    []Channels `json:"channels"`
 }
 
+type PlaylistJson struct {
+	Id           int    `json:"id"`
+	Title        string `json:"title"`
+	Name         string `json:"name"`
+	Show         bool   `json:"show"`
+	Picture      string `json:"picture"`
+	Format       string `json:"format"`
+	Link         string `json:"link"`
+	Width        int    `json:"width"`
+	Height       int    `json:"height"`
+	Size         string `json:"-"`
+	Duration     int    `json:"-"`
+	Code         int    `json:"code"`
+	ErrorMessage string `json:"error"`
+}
+
+type ErrorMess struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
 // func init() {
 // }
 
+// IsExitsSrc
+func (this *Channels) IsExitsSrc(filename string) (string, error) {
+	dir := filepath.Dir(this.DirSrc)
+	fname := filepath.Join(dir, filename)
+	if _, err := os.Stat(fname); os.IsNotExist(err) {
+		return fname, err
+	} else {
+		return fname, nil
+	}
+}
+
+// IsExitsVod
+func (this *Channels) IsExitsVod(filename string) (string, error) {
+	dir := filepath.Dir(this.DirVideoVod)
+	fname := filepath.Join(dir, filename)
+	if _, err := os.Stat(fname); os.IsNotExist(err) {
+		return fname, err
+	} else {
+		return fname, nil
+	}
+}
+
+// isExitsTs Retun Full Path
+func (this *Channels) isExitsTs(filename string) (string, error) {
+	dir := filepath.Dir(this.DirTs)
+	fname := filepath.Join(dir, filename)
+	if _, err := os.Stat(fname); os.IsNotExist(err) {
+		return fname, err
+	} else {
+		return fname, nil
+	}
+}
+
+func (this *Channels) SetTranscode(name string) (err error) {
+	if _, err := this.isExitsTs(name); err != nil {
+		return err
+	} else {
+		this.Count++
+		this.Files = append(this.Files, name)
+		return nil
+	}
+}
+
 func (this *Channels) SetPlayList(name string) (err error) {
-	return nil
+	if _, err := this.isExitsTs(name); err != nil {
+		return err
+	} else {
+		this.Count++
+		this.Files = append(this.Files, name)
+		return nil
+	}
+}
+
+func (c *Channels) GetPlayList() (err error) {
+	var p []PlaylistJson
+	u := fmt.Sprintf("%s/pl/%d/%s/noresize", c.ApiUrl, c.Id, c.Format)
+	err = httplib.Get(u).SetTimeout(100*time.Second, 30*time.Second).ToJson(&p)
+	// req := httplib.Get(u).SetTimeout(100*time.Second, 30*time.Second)
+	// res, err := req.Bytes()
+	// json.Unmarshal(res, &p)
+	for _, f := range p {
+		c.Files = append(c.Files, f.Picture)
+	}
+
+	// fmt.Print("---------", c.Files)
+	return err
+}
+
+func (c *Channels) SavePlayList(name string) (err error) {
+	// dir := filepath.Dir(c.DirConfig)
+	fname := filepath.Join(c.DirConfig, name)
+	// fmt.Println("Write file:", fname)
+	f, err := os.Create(fname)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	w := bufio.NewWriter(f)
+	for _, i := range c.Files {
+		// _, err := w.WriteString(fmt.Sprintln(i))
+		if _, err := os.Stat(i); err == nil {
+			if _, err := w.WriteString(filepath.Base(i) + "\n"); err != nil {
+				return err
+			}
+		}
+	}
+	w.Flush()
+	return err
+}
+
+func (c *Channels) InitChannel() {
+	if _, err := os.Stat(c.DirConfig); err != nil {
+		if err := os.MkdirAll(c.DirConfig, 0777); err != nil {
+			c.Errors.Message = fmt.Sprint("mkdir: ", c.DirConfig, " Error: ", err)
+		}
+	}
+	if _, err := os.Stat(c.DirTs); err != nil {
+		if err := os.MkdirAll(c.DirTs, 0777); err != nil {
+			c.Errors.Message = fmt.Sprint("mkdir: ", c.DirTs, " Error: ", err)
+		}
+	}
 }
